b09bb9728c0df865c657476b1e31e8d681497c11
johannes60-sk <garilsenan@gmail.com>
2023-11-16 17:20:52 +0100

    add module database | created entity Post

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
- 
+import { ConfigModule } from '@nestjs/config';  // Permet de configurer des variables d’environnement
+import * as Joi from '@hapi/joi'; // permet de valider nos variables d’environnement en definisant un schema de validation
+import { DatabaseModule } from './database/database.module';
+
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    ConfigModule.forRoot({    // Tout ceci permet de configurer un schema de validation pour les variables d'env
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      })
+    }),
+    DatabaseModule
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/database/database.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+ 
+
+//Dès que nous créons un fichier .env à la racine de notre application,
+// NestJS les injecte dans un ConfigSerivice
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({  //useFactory permet d’accéder aux variables d’environnement
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [
+          __dirname + '/../**/*.entity.ts',
+        ],
+        synchronize: true,
+      })
+    }),
+  ],
+})
+export class DatabaseModule {}
